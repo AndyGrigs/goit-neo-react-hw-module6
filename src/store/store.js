352d8b1db5404638d5d450de3bf209005cef5942
AndyGrigs <97991761+AndyGrigs@users.noreply.github.com>
2025-05-21 12:27:01 +0200

    add store config

---
+++ src/store/store.js
@@ -0,0 +1,24 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from 'redux-persist/lib/storage';
+
+const contactsStore = {
+    key: 'contacts',
+    storage,
+    whitelist: ['items']
+}
+
+const persistedContactsReducer = persistReducer(contactsStore, contactsReducer);
+
+export const store = configureStore({
+  reducer: {
+    contacts: persistedContactsReducer,
+    filters: filtersReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
